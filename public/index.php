919a333c88eda0f47f4c83821100b831731f2ef6
gribov_pi <gribov_pi@sevesk.ru>
2021-03-21 19:25:09 +0300

    - собираю каркас

--- public/index.php
+++ public/index.php
@@ -18,3 +18,31 @@ require_once WUO_ROOT.'/../vendor/autoload.php';
 spl_autoload_register(function ($class_name) {
     require_once WUO_ROOT.'/../class/'.$class_name.'.php';
 });
+
+
+$view= ClearPath(_GET("view"));
+//die($view);
+if ($view!=""){  
+  if ((is_file(WUO_ROOT."/../controller/view/".$view.".php")==false) and (is_file(WUO_ROOT."/../controller/client/".$view.".php")==false)) {$view="service/404";};
+   // сначала загружаю "не видимую часть если есть" (подготовка view)   
+    if (is_file(WUO_ROOT."/../controller/app/".$view.".php")!=false){
+        require_once WUO_ROOT."/../controller/app/".$view.".php";
+    };
+  // а теперь - отображение
+  require_once WUO_ROOT."/../controller/view/index.php";
+};
+
+$server= ClearPath(_GET("server"));
+if ($server!=""){
+  if (is_file(WUO_ROOT."/../controller/server/".$server.".php")==false) {
+    $view="service/404";      
+    require_once WUO_ROOT."/../controller/view/index.php";
+  } else {
+    require_once WUO_ROOT."/../controller/server/$server.php";
+  };
+};    
+
+if (($view=="") and (($server==""))){
+  $view="index";  
+  require_once WUO_ROOT."/../controller/view/index.php";  
+};
